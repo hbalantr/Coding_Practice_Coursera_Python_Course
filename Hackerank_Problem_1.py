849e494105da2ef92106824ae0f59d18c544e04f
harshitbsai <hbalantr@uwyo.edu>
2025-04-02 19:39:32 -0600

    Create Hackerank_Problem_1.py

---
+++ Hackerank_Problem_1.py
@@ -0,0 +1,9 @@
+The provided code stub reads an integer,n , from STDIN. For all non-negative integers i < n , print i(square). 
+
+if __name__ == '__main__':
+    n = int(input())
+    i=0
+    while i <n:
+        print(i**2)
+        i=i+1
+        
